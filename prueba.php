4ff2aee736ca9912a265b3ac5b8b29f16a1267c6
Rafael Cedano Cairo <rafaelccairo@hotmail.com>
2021-12-02 17:30:54 -0400

    Agregue un H2

--- prueba.php
+++ prueba.php
@@ -19,7 +19,7 @@
 </head>
 
 <body>
-
+<h2>Header</h2>
 <h1>Hola mundo</h1>
     <!-- jQuery CDN - Slim version (=without AJAX) -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>

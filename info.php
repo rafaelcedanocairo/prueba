210cba9a0cd0bdf20f41c248198b31ec16e2b12d
Rafael Cedano Cairo <rafaelccairo@hotmail.com>
2022-01-07 09:24:16 -0400

    agregue un h2

--- info.php
+++ info.php
@@ -3,4 +3,5 @@ header("Content-disposition: attachment; filename=Documento.xlsx");
 header("Content-type: MIME");
 readfile("Documento.xlsx");
 echo "<script>window.close();</script>";
+
 ?>
--- prueba.php
+++ prueba.php
@@ -20,6 +20,7 @@
 
 <body>
 <h3>Experimento</h3>
+<h2>Hola</h2>
 <h1>Hola mundo</h1>
     <!-- jQuery CDN - Slim version (=without AJAX) -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>

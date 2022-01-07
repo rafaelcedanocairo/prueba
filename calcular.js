6577df34a8bcd138993ce6c039807ad9fb985985
Rafael Cedano Cairo <rafaelccairo@hotmail.com>
2022-01-07 10:24:16 -0400

    Borre todo solo para cargar un archivo html

--- calcular.js
+++
@@ -1,8 +0,0 @@
-const peso = document.getElementById('peso');
-
-peso.addEventListener("keyup", function(event) {
-    if (event.keyCode === 13) {
-     event.preventDefault();
-     alert(peso.value)
-    }
-  });
--- info.php
+++
@@ -1,7 +0,0 @@
-<?php
-header("Content-disposition: attachment; filename=Documento.xlsx");
-header("Content-type: MIME");
-readfile("Documento.xlsx");
-echo "<script>window.close();</script>";
-
-?>
